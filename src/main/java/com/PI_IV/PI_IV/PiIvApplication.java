031a960620ae3cda9ce24d894bf35807f23653e9
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-02-18 14:35:01 -0300

    SpringBoot

---
+++ src/main/java/com/PI_IV/PI_IV/PiIvApplication.java
@@ -0,0 +1,13 @@
+package com.PI_IV.PI_IV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiIvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PI_IV/PI_IV/PiIvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PI_IV.PI_IV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
