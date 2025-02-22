09f4db315f394d321d2ddf46bcf1f0beddc93ea3
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-02-22 16:09:24 -0300

    refactor: melhorar organização no UsuarioController

--- src/main/java/com/PI_IV/controller/UsuarioController.java
+++ src/main/java/com/PI_IV/controller/UsuarioController.java
@@ -1,14 +1,22 @@
 package com.PI_IV.controller;
 
+import com.PI_IV.DAO.InterfaceUsuario;
+import com.PI_IV.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class UsuarioController {
 
+    @Autowired
+    private InterfaceUsuario dao;
+
+    //meotodo para procurar todos os usuarios do banco
     @GetMapping("/usuarios")
-    public String teste(){
-        return "Acessando a API do projeto";
+    public List<Usuario> listaUsuarios (){
+        return (List<Usuario>) dao.findAll();
     }
 
 }
