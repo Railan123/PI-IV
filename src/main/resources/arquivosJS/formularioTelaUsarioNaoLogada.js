a625950e547776b37a0ebf4b153efdd3f7507f1e
Guilherme Domiciano de Viveiros <guilhermedmc00@gmail.com>
2025-02-26 13:52:23 -0300

    Formulario da tela de login conectando com o backend

---
+++ src/main/resources/arquivosJS/formularioTelaUsarioNaoLogada.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+
+
+        let email = document.getElementById("email").value.trim();
+        let senha = document.getElementById("senha").value.trim();
+
+        fetch("http://localhost:8080/usuarios/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, senha })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Email ou senha inválidos!");
+                }
+                return response.text();
+            })
+            .then(message => {
+                alert(message);
+                window.location.href = "cadastrarUsuario.html"; // Redireciona o admin para o painel
+            })
+            .catch(error => {
+                alert(error.message);
+            });
+    });
+});
