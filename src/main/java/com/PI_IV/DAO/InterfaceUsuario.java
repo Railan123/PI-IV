11ba01727a17715bb634dd68c66ce7ad366db72b
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-02-28 00:34:24 -0300

    Atualiza InterfaceUsuario com métodos de busca por ID, email e nome

--- src/main/java/com/PI_IV/DAO/InterfaceUsuario.java
+++ src/main/java/com/PI_IV/DAO/InterfaceUsuario.java
@@ -2,6 +2,21 @@ package com.PI_IV.DAO;
 
 import com.PI_IV.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface InterfaceUsuario extends CrudRepository <Usuario, Integer> {
+
+    // Busca usuário pelo ID
+    Optional<Usuario> findById(Integer id);
+
+    // Busca usuário pelo email
+    Optional<Usuario> findByEmail(String email);
+
+    // Busca usuário pelo nome
+    Optional<Usuario> findByNome(String nome);
 }
+
+
