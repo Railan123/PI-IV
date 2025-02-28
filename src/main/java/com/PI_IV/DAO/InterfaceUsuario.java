c74360b98e794858f05d8222daeefaf52a65c7e7
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-02-28 18:34:59 -0300

    Atualiza a interface de acesso a dados do usuário para refletir mudanças na autenticação e gerenciamento de usuários.

--- src/main/java/com/PI_IV/DAO/InterfaceUsuario.java
+++ src/main/java/com/PI_IV/DAO/InterfaceUsuario.java
@@ -2,6 +2,8 @@ package com.PI_IV.DAO;
 
 import com.PI_IV.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
-public interface InterfaceUsuario extends CrudRepository <Usuario, Integer> {
+public interface InterfaceUsuario extends CrudRepository<Usuario, Integer> {
+    Optional<Usuario> findByEmail(String email);
 }
