96a064efaab4899164c61352f1e61bfd9686988b
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-03-07 15:29:01 -0300

    Adicionar novos métodos para gerenciar usuários

--- src/main/java/com/PI_IV/DAO/InterfaceUsuario.java
+++ src/main/java/com/PI_IV/DAO/InterfaceUsuario.java
@@ -6,4 +6,7 @@ import java.util.Optional;
 
 public interface InterfaceUsuario extends CrudRepository<Usuario, Integer> {
     Optional<Usuario> findByEmail(String email);
+
+    Optional<Usuario> findByCpf(String cpf);
+
 }
