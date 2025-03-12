e82b0465dd9c519ee772f4231e167747083805f3
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-03-12 11:58:40 -0300

    Cria modelo Produto

---
+++ src/main/java/com/PI_IV/model/Produto.java
@@ -0,0 +1,40 @@
+package com.PI_IV.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 200)
+    private String nome;
+
+    @Column(precision = 2, scale = 1)
+    private BigDecimal avaliacao;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer quantidadeEstoque = 0;
+
+    @Column(length = 255)
+    private String imagemPadrao;
+
+    @Column(nullable = false)
+    private boolean ativo = true;
+}
