a4880494ef487dcc7abfbf8f9e182b438cff3e99
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-03-14 02:54:15 -0300

    Modificações no modelo Produto

--- src/main/java/com/PI_IV/model/Produto.java
+++ src/main/java/com/PI_IV/model/Produto.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 @Entity
@@ -13,15 +14,6 @@ import java.math.BigDecimal;
 @Data
 public class Produto {
 
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -29,21 +21,32 @@ public class Produto {
     @Column(nullable = false, length = 200)
     private String nome;
 
-    @Column(precision = 2, scale = 1)
+    @Column(nullable = false, precision = 2, scale = 1)
     private BigDecimal avaliacao;
 
-    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(nullable = false, length = 255)
     private String descricao;
 
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal preco;
 
     @Column(nullable = false)
-    private Integer quantidadeEstoque = 0;
-
-    @Column(length = 255)
-    private String imagemPadrao;
+    private Integer quantidadeEstoque=0;
 
     @Column(nullable = false)
     private boolean ativo = true;
+
+    @Column(nullable = true, columnDefinition = "LONGTEXT")
+    private String imagemPadrao;
+
+    @Lob
+    @Column(name = "imagem_blob")
+    private byte[] imagemBlob;
+
+    public void setAvaliacao(BigDecimal avaliacao) {
+        if (avaliacao.compareTo(BigDecimal.ONE) < 0 || avaliacao.compareTo(BigDecimal.valueOf(5)) > 0) {
+            throw new IllegalArgumentException("A avaliação deve estar entre 1 e 5.");
+        }
+        this.avaliacao = avaliacao;
+    }
 }
