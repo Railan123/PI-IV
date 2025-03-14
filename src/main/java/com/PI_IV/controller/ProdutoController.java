74fc5d905deb7f9c2a5da38ed789f939f48d1f53
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-03-14 02:54:14 -0300

    Ajustes no ProdutoController

--- src/main/java/com/PI_IV/controller/ProdutoController.java
+++ src/main/java/com/PI_IV/controller/ProdutoController.java
@@ -2,8 +2,11 @@ package com.PI_IV.controller;
 
 import com.PI_IV.model.Produto;
 import com.PI_IV.service.ProdutoService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,8 +35,7 @@ public class ProdutoController {
     @GetMapping("/{id}")
     public ResponseEntity<Produto> buscarPorId(@PathVariable Integer id) {
         Optional<Produto> produto = service.buscarPorId(id);
-        return produto.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+        return produto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
@@ -54,16 +56,24 @@ public class ProdutoController {
 
     @PutMapping("/ativarDesativar/{id}")
     public ResponseEntity<Produto> ativarDesativarProduto(@PathVariable Integer id) {
-        Optional<Produto> produtoOpt = service.buscarPorId(id);
-
-        if (produtoOpt.isPresent()) {
-            Produto produto = produtoOpt.get();
-            produto.setAtivo(!produto.isAtivo()); // Alterna entre ativo/inativo
-            Produto produtoAtualizado = service.salvar(produto);
-            return ResponseEntity.ok(produtoAtualizado); // Retorna JSON válido
-        }
-
-        return ResponseEntity.notFound().build();
+        Optional<Produto> produtoOpt = service.ativarDesativar(id);
+        return produtoOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PostMapping("/{id}/imagem")
+    public ResponseEntity<Produto> salvarImagem(@PathVariable Integer id, @RequestParam("imagem") MultipartFile imagem) {
+        Produto produtoAtualizado = service.salvarImagem(id, imagem);
+        return produtoAtualizado != null ? ResponseEntity.ok(produtoAtualizado) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/imagem")
+    public ResponseEntity<byte[]> recuperarImagem(@PathVariable Integer id) {
+        byte[] imagem = service.recuperarImagem(id);
+        if (imagem == null) return ResponseEntity.notFound().build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "image/png");
+
+        return new ResponseEntity<>(imagem, headers, HttpStatus.OK);
+    }
 }
